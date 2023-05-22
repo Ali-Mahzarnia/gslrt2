26d4cc92429e89ec31141da1c68ffefbdd46d9f6
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2023-05-22 14:55:41 -0400

    Add files via upload

--- van.R
+++ van.R
@@ -12,7 +12,12 @@ ME_path = '/Users/ali/Desktop/april23/GSLRT/van/mmse_mf_intrc_apoe_no_PMI.xlsx'
 ME = as.data.frame(read.xlsx2(ME_path, sheetIndex = 1))
 
 # filtered :
-AB = AB [, as.numeric(AB$p)<=0.05] 
-LT = LT [, as.numeric(LT$p)<=0.05] 
-ME = ME [, as.numeric(ME$p)<=0.05] 
+AB = AB [ as.numeric(AB$p)<=0.05, ] 
+LT = LT [ as.numeric(LT$p)<=0.05, ] 
+ME = ME [ as.numeric(ME$p)<=0.05, ] 
 
+intersect(AB$GS, LT$GS) 
+intersect(LT$GS, ME$GS) 
+intersect(ME$GS, AB$GS) 
+
+intersect(LT$GS , intersect(ME$GS, AB$GS) )
