ef532bddea54e13847e5e36098ff9899a6d7671c
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2023-05-26 23:05:40 -0400

    Add files via upload

--- barplot.R
+++ barplot.R
@@ -14,8 +14,15 @@ ME = as.data.frame(read.xlsx2(ME_path, sheetIndex = 1))
 
 # # filtered :
  AB = AB [ abs(as.numeric(AB$s))>2, ] 
+ AB$s =as.numeric(AB$s)
+ AB$GS =gsub( "_", " ", AB$GS)
  LT = LT [ abs(as.numeric(LT$s))>1.5, ] 
+ LT$s =as.numeric(LT$s)
+ LT$GS =gsub( "_", " ", LT$GS)
  ME = ME [ abs(as.numeric(ME$s))>3, ] 
+ ME$s =as.numeric(ME$s)
+ ME$GS =gsub( "_", " ", ME$GS)
+ 
  
 
 
@@ -28,10 +35,12 @@ library(GOplot)
  ggplot(AB, aes(reorder(GS, s), s, fill = adjPvalue)) +
    geom_col() +
    scale_fill_manual(values = cols) +
-   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+         panel.background = element_rect(fill = "white")) +
    coord_flip() +
-   labs(x="Pathway", y="Normalized Enrichment Score(Test statistcs)",
-        title="Hallmark pathways Enrichment Score from Log-Tangles")
+   labs(x="Pathway", y="Normalized Enrichment Score (Test statistcs)",
+        title="Hallmark pathways Enrichment Score from Amyloid beta") 
+
  ggsave( "AB.pdf", plot = last_plot(), device='pdf', scale=1, width=20, height=10, unit=c("in"), dpi=200)
  
 LT$adjPvalue <- ifelse(LT$p <= 0.1, "significant", "non-significant")
@@ -39,10 +48,11 @@ cols <- c("non-significant" = "grey", "significant" = "red")
 ggplot(LT, aes(reorder(GS, s), s, fill = adjPvalue)) +
   geom_col() +
   scale_fill_manual(values = cols) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+        panel.background = element_rect(fill = "white")) +
   coord_flip() +
   labs(x="Pathway", y="Normalized Enrichment Score(Test statistcs)",
-       title="Hallmark pathways Enrichment Score from Log-Tangles")
+       title="Hallmark pathways Enrichment Score from Log(Tangles)")
 ggsave( "Log_tangles.pdf", plot = last_plot(), device='pdf', scale=1, width=20, height=10, unit=c("in"), dpi=200)
 
 
@@ -53,10 +63,11 @@ cols <- c("non-significant" = "grey", "significant" = "red")
 ggplot(ME, aes(reorder(GS, s), s, fill = adjPvalue)) +
   geom_col() +
   scale_fill_manual(values = cols) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+        panel.background = element_rect(fill = "white")) +
   coord_flip() +
   labs(x="Pathway", y="Normalized Enrichment Score(Test statistcs)",
-       title="Hallmark pathways Enrichment Score from Log-Tangles")
+       title="Hallmark pathways Enrichment Score from MMSE")
 
 ggsave( "MMSE.pdf", plot = last_plot(), device='pdf', scale=1, width=20, height=10, unit=c("in"), dpi=200)
 
